c405b626a89c41756e7d396d7c99fecc8de93e46
Filip Groh <146720108+Filip-Groh@users.noreply.github.com>
2024-01-15 13:34:59 +0100

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,26 @@
+radio.setGroup(69)
+radio.setTransmitSerialNumber(true)
+radio.setTransmitPower(7)
 
+let sn = 0
+
+basic.forever(() => {
+    send()
+    basic.pause(1 / 60)
+    serial.writeValue("mySerialNumber", control.deviceSerialNumber())
+})
+
+function send() {
+    radio.sendNumber(111)
+}
+
+radio.onReceivedNumber(function(receivedNumber: number) {
+    let serialNumber = radio.receivedPacket(RadioPacketProperty.SerialNumber)
+    if (serialNumber === sn) {
+        serial.writeNumber(receivedNumber)
+    }
+})
+
+radio.onReceivedValue(function(name: string, value: number) {
+    serial.writeValue(name, value)
+})
